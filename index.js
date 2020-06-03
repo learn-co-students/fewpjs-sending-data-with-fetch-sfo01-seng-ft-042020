e1eaedc755d99e072c386175ad2a22a42217e92e
tharveybrown <tharveybrown@gmail.com>
2020-06-03 13:52:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// // Add your code here
+function submitData(userName, email) {
+  const destURL = 'http://localhost:3000/users'
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }, 
+    body: JSON.stringify({
+      name: userName,
+      email: email
+    })
+  }
+  return fetch(destURL, configObj)
+    .then(resp => resp.json())
+    .then( obj => document.body.innerHTML = obj.id)
+    .catch(function(err) {
+      document.body.innerHTML = err.message;
+      
+    })
+}
